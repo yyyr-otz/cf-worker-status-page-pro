b0f6d30381a156bc8cc78b15a0dfda1f061ef53b
yyyr-otz <32903075+yyyr-otz@users.noreply.github.com>
2025-05-03 23:15:25 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -2,15 +2,15 @@ import type { Config } from './types'
 
 export const config: Config = {
   settings: {
-    title: "yyyr's Status Page Pro",
+    title: 'Status Page Pro',
     url: 'https://cf-worker-status-page-pro-production.yyyrspeed.workers.dev',
     displayDays: 7,
     collectResponseTimes: true,
   },
   monitors: [
     {
-      id: 'nezha@koyeb',
-      url: 'https://nezha-yyyrspeed.koyeb.app/m',
+      id: 'nezha-koyeb',
+      url: 'https://nezha-yyyrspeed.koyeb.app',
       description: '哪吒@koyeb',
       followRedirect: false,
     }
